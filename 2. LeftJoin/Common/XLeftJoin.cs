bbb804667426de701e05e176b13c3f2f3d2dfce9
Evgeny Surikov <evgeny.surikov@altenar.com>
2020-01-23 17:32:53 +0300

    change GetEnumerator

--- 2. LeftJoin/Common/XLeftJoin.cs
+++ 2. LeftJoin/Common/XLeftJoin.cs
@@ -14,7 +14,7 @@ namespace Common
 
         public static IEnumerable<Item1> GetYield(IEnumerable<Item1> source, IEnumerable<Item2> join)
         {
-            foreach(var x in source)
+            foreach (var x in source)
             {
                 var joined = new Item1 { ID = x.ID, Value = x.Value, Details = join.FirstOrDefault(x => x.ID == x.ID)?.Details };
                 yield return joined;
@@ -24,40 +24,35 @@ namespace Common
 
     public class XEnumerable : IEnumerable<Item1>
     {
-        private readonly IEnumerator<Item1> _enumerator;
+        private readonly IEnumerable<Item2> _join;
+        private readonly IEnumerable<Item1> _source;
 
         public XEnumerable(IEnumerable<Item1> source, IEnumerable<Item2> join)
-        {
-            _enumerator = new XEnumerator(source, join);
-        }
-
-        public IEnumerator<Item1> GetEnumerator()
-        {
-            return _enumerator;
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return _enumerator;
-        }
-    }
-
-    public class XEnumerator : IEnumerator<Item1>
-    {
-        private IEnumerable<Item2> _join;
-        private IEnumerable<Item1> _source;
-        private IEnumerator<Item1> _sourceEnumerator;
-        private Item1 _current;
-
-        public XEnumerator(IEnumerable<Item1> source, IEnumerable<Item2> join)
         {
             _join = join;
             _source = source;
         }
 
+        public IEnumerator<Item1> GetEnumerator() => new XEnumerator(_source.GetEnumerator(), _join);
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+
+    public class XEnumerator : IEnumerator<Item1>
+    {
+        private readonly IEnumerable<Item2> _join;
+        private readonly IEnumerator<Item1> _sourceEnumerator;
+        private Item1 _current;
+
+        public XEnumerator(IEnumerator<Item1> sourceEnumerator, IEnumerable<Item2> join)
+        {
+            _join = join;
+            _sourceEnumerator = sourceEnumerator;
+        }
+
         private Item1 GetCurrent()
         {
-            if (_current == null && _sourceEnumerator  != null && _sourceEnumerator.Current != null)
+            if (_current == null && _sourceEnumerator.Current != null)
             {
                 _current = new Item1 { ID = _sourceEnumerator.Current.ID, Value = _sourceEnumerator.Current.Value };
                 _current.Details = _join.FirstOrDefault(x => x.ID == _current.ID)?.Details;
@@ -65,15 +60,13 @@ namespace Common
             return _current;
         }
 
-        public Item1 Current => GetCurrent();        
+        public Item1 Current => GetCurrent();
 
         object IEnumerator.Current => GetCurrent();
 
         public void Dispose()
         {
-            // Dispose of unmanaged resources.
             Dispose(true);
-            // Suppress finalization.
             GC.SuppressFinalize(this);
         }
 
@@ -82,18 +75,12 @@ namespace Common
             if (disposing)
             {
                 _sourceEnumerator.Dispose();
-                _sourceEnumerator = null;
             }
         }
 
         public bool MoveNext()
         {
             _current = null;
-            if (_sourceEnumerator == null)
-            {
-                _sourceEnumerator = _source.GetEnumerator();
-            }
-
             return _sourceEnumerator.MoveNext();
         }
 
